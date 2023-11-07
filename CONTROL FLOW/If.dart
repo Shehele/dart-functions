b845190f3cb029ade21b2585043ece2704d9fe2f
Bigboss <ochie@Bigboss>
2023-11-08 02:52:53 +0300

    functions and decision making

---
+++ CONTROL FLOW/If.dart
@@ -0,0 +1,7 @@
+//if statements in dart language
+voidmain() {
+  int num1 = 18;
+  if (num1 >= 18) {
+    print("I CAN VOTE IN RWANDA");
+  }
+}
---
+++ CONTROL FLOW/Ifelseif.dart
@@ -0,0 +1,13 @@
+//if else if statement in dart
+void main() {
+  var math = 65;
+  if (math >= 80) {
+    print("congratulations!!");
+  } else if (math >= 70) {
+    print("good work");
+  } else if (math >= 65) {
+    print("good");
+  } else {
+    print("see Me!");
+  }
+}
